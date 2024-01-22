88ded79234b2fce55d0721d81d2b4cbf41d33d15
Andrew Zolensky <andrewlzolensky@gmail.com>
2024-01-22 13:46:02 -0500

    compiled and ran example c++ program

---
+++ example.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, sum;
+    
+    cout << "Enter two numbers: ";
+    cin >> a;
+    cin >> b;
+    
+    sum = a + b;
+    
+    cout << "The sum is: " << sum << endl;
+    
+    return 0;
+}
+
